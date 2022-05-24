178a4859ab5ad4decdbc7119b73fcd38611aed72
piroyuki666 <shtr3636@gmail.com>
2022-05-24 19:08:45 +0900

    rubocopの実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe User, type: :model do
       it 'emailに@を含まないと登録できない' do
         @user.email = 'testmail'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Email is invalid') 
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'passwordが5文字以下では登録できない' do
         @user.password = '00000'

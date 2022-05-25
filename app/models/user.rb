9f735831e8366e3cca0aba5893ff7ceb076a2bf2
piroyuki666 <shtr3636@gmail.com>
2022-05-25 14:33:13 +0900

    正規表現の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
   validates :birth_date, presence: true
 
-  has_many :items
-  has_many :orders
 end

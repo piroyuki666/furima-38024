ad7322dca91a411eafddd3a2280e3b5cc5feec3a
piroyuki666 <shtr3636@gmail.com>
2022-05-27 17:14:11 +0900

    出品機能にまつわる実装
    ItemControllerの設定
    ActiveHash導入
    バリデーション、アソシエーションの設定
    各カテゴリーの保存
    views/new.htmlの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_info
-    validates :item_category_id
-    validates :item_sales_status_id
-    validates :item_shipping_fee_status_id
-    validates :item_prefecture_id
-    validates :item_scheduled_delivery_id
   end
+
+  validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
 
   #値段は「整数値」かつ「300〜9999999」の範囲に指定
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
@@ -17,4 +18,11 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :item_prefecture
+  belongs_to :item_scheduled_delivery
 end

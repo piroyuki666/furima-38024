f97d44488d67979e3217b593b4710ff6b1744276
piroyuki666 <shtr3636@gmail.com>
2022-05-31 19:51:02 +0900

    itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   validates :item_scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
 
   # 値段は「整数値」かつ「300〜9999999」の範囲に指定
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+  with_options presence: true do
     validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 

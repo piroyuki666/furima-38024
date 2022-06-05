3c6ca1dce1b9b8b7d3f13e2d75bf3d8c876fc880
piroyuki666 <shtr3636@gmail.com>
2022-06-05 10:36:14 +0900

    フォームオブジェクトパターンに置換
    ２つのテーブルへのデータの保存に成功

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,21 +2,24 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
     redirect_to root_path if current_user.id == @item.user_id
   end
 
   def create
-    @order = Order.create(order_params)
-    Address.create(address_params)
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
   end
 
   private
 
   def order_params
-    params.permit.merge(user_id: current_user.id, item_id: params[:item_id])
-  end
-
-  def address_params
-    params.permit(:postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number).merge(order_id: @order.id)
+    params.require(:order_address).permit(:postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,16 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id, :item_id, :city, :addresses
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :item_prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :phone_number, format: { with: /\d{10,11}/, message: "is invalid" }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, item_prefecture_id: item_prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
